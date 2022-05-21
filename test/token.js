55c72dd090d7bc984b05fb522492fa9d99313f93
abiiralbhattarai <bhattarai.abi07@gmail.com>
2022-05-21 17:22:08 +0545

    Created second test and tested sucessfully

--- test/token.js
+++ test/token.js
@@ -7,7 +7,6 @@ describe("Token contract",function (){
 
         const [owner] = await ethers.getSigners();
 
-        console.log("Signers object:",owner);
         const Token = await ethers.getContractFactory("Token");//instance contract
         const hardhatToken = await Token.deploy();//deploy contract
 
@@ -16,6 +15,22 @@ describe("Token contract",function (){
 
         expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);//total supply = 10000
 
-})
+    });
 
+    it("Should transfer tokens between accounts",async function() {
+
+        const [owner,addr1, addr2] = await ethers.getSigners();
+
+        const Token = await ethers.getContractFactory("Token");//instance contract
+        const hardhatToken = await Token.deploy();//deploy contract
+        //Transer 10 tokens from owner to addr1
+
+        await hardhatToken.transfer(addr1.address,10);
+        expect(await hardhatToken.balanceOf(addr1.address)).to.equal(10);
+        
+        //Transfer 5 tokens from addr1 to addr2
+        await hardhatToken.connect(addr1).transfer(addr2.address,5);
+        expect(await hardhatToken.balanceOf(addr2.address)).to.equal(5);
+
+    });
 });
