cda3f8dce4dc9155db213c9fc83409cf163e1d46
abiiralbhattarai <bhattarai.abi07@gmail.com>
2022-05-22 12:20:15 +0545

    Deployed on rinkeby test network using alchemy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,21 @@
+require('dotenv').config();
 require("@nomiclabs/hardhat-waffle");
+
 
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-
 module.exports = {
+  defaultNetwork: "rinkeby",
   solidity: "0.8.4",
-
   networks: {
-    ropsten: {
+    rinkeby: {
       url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [`0x${process.env.ROPSTEN_PRIVATE_KEY}`],
+      accounts: [`0x${process.env.RINKEBY_PRIVATE_KEY}`],
+      gas: 2100000,
+      gasPrice: 8000000000,
+      saveDeployments: true,
     },
   
   }
